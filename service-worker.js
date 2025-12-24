1ba1c0a79d8480c0b2cd8faa0ef6ee3818048ae2
folk0101 <folksurasek0@gmail.com>
2025-12-24 13:24:32 +0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("install", e => self.skipWaiting());
-
-self.addEventListener("notificationclick", e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow("index.html"));
-});
